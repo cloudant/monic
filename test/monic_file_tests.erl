eb28c1c726cd7f10ce684911e15d8bbc1cce11bd
Robert Newson <robert.newson@cloudant.com>
2011-03-18 21:04:51 +0000

    indent

--- test/monic_file_tests.erl
+++ test/monic_file_tests.erl
@@ -20,40 +20,40 @@
 
 all_test_() ->
     {foreach,
-    fun() ->
-        file:delete("foo.monic"),
-        file:delete("foo.monic.idx"),
-        {ok, Pid} = monic_file:open("foo.monic"),
-        Pid end,
-    fun(Pid) -> monic_file:close(Pid) end,
-    [
-        fun add_single_hunk/1,
-        fun add_multi_hunk/1,
-        fun add_multi_items/1,
-        fun overflow/1,
-        fun underflow/1
-    ]}.
+     fun() ->
+             file:delete("foo.monic"),
+             file:delete("foo.monic.idx"),
+             {ok, Pid} = monic_file:open("foo.monic"),
+             Pid end,
+     fun(Pid) -> monic_file:close(Pid) end,
+     [
+      fun add_single_hunk/1,
+      fun add_multi_hunk/1,
+      fun add_multi_items/1,
+      fun overflow/1,
+      fun underflow/1
+     ]}.
 
 add_single_hunk(Pid) ->
     {"add an item in one hunk",
-    fun() ->
-        StreamBody = {<<"123">>, done},
-        ?assertMatch(ok, monic_file:add(Pid, <<"foo">>, ?COOKIE, 3, StreamBody)),
-        ?assertMatch({ok, StreamBody}, monic_file:read(Pid, <<"foo">>,  ?COOKIE))
-    end}.
+     fun() ->
+             StreamBody = {<<"123">>, done},
+             ?assertMatch(ok, monic_file:add(Pid, <<"foo">>, ?COOKIE, 3, StreamBody)),
+             ?assertMatch({ok, StreamBody}, monic_file:read(Pid, <<"foo">>,  ?COOKIE))
+     end}.
 
 add_multi_hunk(Pid) ->
     {"add an item in multiple hunks",
-    fun() ->
-        StreamBody = {<<"123">>, fun() -> {<<"456">>, done} end},
-        ?assertMatch(ok, monic_file:add(Pid, <<"foo">>, ?COOKIE, 6, StreamBody))
-    end}.
+     fun() ->
+             StreamBody = {<<"123">>, fun() -> {<<"456">>, done} end},
+             ?assertMatch(ok, monic_file:add(Pid, <<"foo">>, ?COOKIE, 6, StreamBody))
+     end}.
 
 add_multi_items(Pid) ->
     [?_assertMatch(ok, monic_file:add(Pid, <<"foo">>, ?COOKIE, 3, {<<"123">>, done})),
-    ?_assertMatch(ok, monic_file:add(Pid, <<"bar">>, ?COOKIE, 3, {<<"456">>, done})),
-    ?_assertMatch(ok, monic_file:add(Pid, <<"baz">>, ?COOKIE, 3, {<<"789">>, done})),
-    ?_assertMatch(ok, monic_file:add(Pid, <<"foobar">>, ?COOKIE, 3, {<<"abc">>, done}))].
+     ?_assertMatch(ok, monic_file:add(Pid, <<"bar">>, ?COOKIE, 3, {<<"456">>, done})),
+     ?_assertMatch(ok, monic_file:add(Pid, <<"baz">>, ?COOKIE, 3, {<<"789">>, done})),
+     ?_assertMatch(ok, monic_file:add(Pid, <<"foobar">>, ?COOKIE, 3, {<<"abc">>, done}))].
 
 overflow(Pid) ->
     Res = monic_file:add(Pid, <<"foo">>, ?COOKIE, 3, {<<"1234">>, done}),
