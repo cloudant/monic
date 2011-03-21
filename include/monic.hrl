adb0db91f804f84c55852b2d27d6726ae3b193dd
Robert Newson <robert.newson@cloudant.com>
2011-03-21 20:07:45 +0000

    indent monic.hrl

--- include/monic.hrl
+++ include/monic.hrl
@@ -1,22 +1,22 @@
 -define(BUFFER_SIZE, 64*1024).
 
 -record(index, {
-    key :: binary(),
-    cookie :: integer(),
-    location :: integer(),
-    size :: integer(),
-    last_modified,
-    deleted=false :: boolean()
-}).
+          key :: binary(),
+          cookie :: integer(),
+          location :: integer(),
+          size :: integer(),
+          last_modified,
+          deleted=false :: boolean()
+         }).
 
 -record(header, {
-    key :: binary(),
-    cookie :: integer(),
-    size :: integer(),
-    last_modified,
-    deleted=false :: boolean()
-}).
+          key :: binary(),
+          cookie :: integer(),
+          size :: integer(),
+          last_modified,
+          deleted=false :: boolean()
+         }).
 
 -record(footer, {
-    sha :: binary()
-}).
+          sha :: binary()
+         }).
