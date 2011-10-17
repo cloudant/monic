9b7670d53ee40efea57c777f044b3de74c66e6de
Robert Newson <robert.newson@cloudant.com>
2011-10-17 15:55:25 +0100

    expose /container/_compact feature.

---
+++ src/monic_container_compact_resource.erl
@@ -0,0 +1,39 @@
+%% Copyright 2011 Cloudant
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. You may obtain a copy of
+%% the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+%% License for the specific language governing permissions and limitations under
+%% the License.
+
+-module(monic_container_compact_resource).
+-export([init/1,
+         allowed_methods/2,
+         process_post/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+-include("monic.hrl").
+
+init(ConfigProps) ->
+    {ok, ConfigProps}.
+
+allowed_methods(ReqData, Context) ->
+    {['POST'], ReqData, Context}.
+
+process_post(ReqData, Context) ->
+    Result = case monic_utils:open(ReqData, Context) of
+        {ok, Pid} ->
+            spawn(fun() -> monic_file:compact(Pid) end),
+            true;
+        _ ->
+            false
+    end,
+    {Result, ReqData, Context}.
+
+                
